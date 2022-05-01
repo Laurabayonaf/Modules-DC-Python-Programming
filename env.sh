77114ebad86e5b893d2efec51bfd51ef48134c5d
Laura Sofia Bayona <88544555+Laurabayonaf@users.noreply.github.com>
2022-05-01 18:49:31 +1000

    Updated information SH Public File
    I have updated my details

--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
 export GIT_PARENT_DIR=~
-export GIT_REPO_NAME=gitjupyter
+export GIT_REPO_NAME=Modules-DC-Python-Programming
 export GIT_BRANCH_NAME=master
-export GIT_USER=sat28
-export GIT_EMAIL=anand.shaleen@gmail.com
-export GITHUB_ACCESS_TOKEN=#access-token from github developer settings
-export GIT_USER_UPSTREAM=sat28
+export GIT_USER=Laurabayonaf
+export GIT_EMAIL=laurabayonaf@gmail.com
+export GITHUB_ACCESS_TOKEN=#
+export GIT_USER_UPSTREAM=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
+
