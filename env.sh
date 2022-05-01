3f24fb36016928c186635f4a34fe7b8e281872c6
Laura Sofia Bayona <88544555+Laurabayonaf@users.noreply.github.com>
2022-05-01 18:46:18 +1000

    Creating SH File with the Git Parameters

---
+++ env.sh
@@ -0,0 +1,7 @@
+export GIT_PARENT_DIR=~
+export GIT_REPO_NAME=gitjupyter
+export GIT_BRANCH_NAME=master
+export GIT_USER=sat28
+export GIT_EMAIL=anand.shaleen@gmail.com
+export GITHUB_ACCESS_TOKEN=#access-token from github developer settings
+export GIT_USER_UPSTREAM=sat28
